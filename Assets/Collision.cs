03adb85f1f904d691a1913c7e72310ec11128297
Xiubo <906672365@qq.com>
2020-01-14 11:44:34 -0700

    Player & sword pickup prefabs

---
+++ Assets/Collision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    Camera thisCam;
+
+    private void Start()
+    {
+        thisCam = GetComponent<Camera>();
+    }
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            Debug.Log(hit.collider.name);
+            
+        }
+    }
+}
