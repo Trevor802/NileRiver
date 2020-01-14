6fde72ba2a5a028dc0ccade63f1eda4db8faf7d1
Jingyi Zhang <jerryzhangjingyi@gmail.com>
2020-01-14 11:08:46 -0700

    sword can break

---
+++ Assets/Scripts/SlotAvailability.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotAvailability : MonoBehaviour
+{
+    public bool hasWeapon = false;
+    public bool weaponBroken = false;
+
+    public GameObject weapon;
+    private void Update()
+    {
+        if(weaponBroken == true)
+        {
+            weapon.SetActive(false);
+            weaponBroken = false;
+            hasWeapon = false;
+        }
+    }
+    public bool checkSlotAviliability()
+    {
+        return hasWeapon;
+    }
+
+}
