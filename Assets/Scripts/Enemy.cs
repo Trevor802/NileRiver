10eece5a5d102d3636bab943d6d91adb3b3e53be
Jingyi Zhang <jerryzhangjingyi@gmail.com>
2020-01-16 09:18:54 -0700

    added skeleton to enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private int HP = 10;
-    private Transform weapon;
+    public Transform weapon;
+    private float delay = 0f;
     private enum Direction
     {
         left,
@@ -17,28 +18,36 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        weapon = transform.Find("weapon");
+        //weapon = transform.Find("weapon");
         dir = Direction.left;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (weapon.localPosition.x > -4.87f && dir == Direction.left)
+        //print(Time.deltaTime);
+        delay += Time.deltaTime;
+        //if (weapon.localPosition.x > 2.2f && dir == Direction.left)
+        if(delay < 0.5f && dir == Direction.left)
         {
             weapon.localPosition = new Vector3(weapon.localPosition.x - 0.03f, weapon.localPosition.y, weapon.localPosition.z);
         }
-        else if (weapon.localPosition.x <= -4.87f && dir == Direction.left)
+        //else if (weapon.localPosition.x <= 2.2f && dir == Direction.left)
+        else if(delay >= 0.5 && dir == Direction.left)
         {
             dir = Direction.right;
+            delay = 0f;
         }
-        if (weapon.localPosition.x < - 3.89f && dir == Direction.right)
+        //if (weapon.localPosition.x < 1.31f && dir == Direction.right)
+        if (delay < 0.5f && dir == Direction.right)
         {
             weapon.localPosition = new Vector3(weapon.localPosition.x + 0.03f, weapon.localPosition.y, weapon.localPosition.z);
         }
-        else if (weapon.localPosition.x >= -3.89f && dir == Direction.right)
+        //else if (weapon.localPosition.x >= 1.31f && dir == Direction.right)
+        else if(delay >= 0.5 && dir == Direction.right)
         {
             dir = Direction.left;
+            delay = 0f;
         }
 
     }
