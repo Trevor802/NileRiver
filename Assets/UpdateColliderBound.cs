4c40c3e42cec77c1f4261df275c818a78e87a4ca
Trevor Alex <pascerveau@gmail.com>
2020-01-16 15:41:18 -0700

    Game level design

---
+++ Assets/UpdateColliderBound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(BoxCollider2D))]
+public class UpdateColliderBound : MonoBehaviour
+{
+    private void Awake()
+    {
+        runInEditMode = true;
+    }
+
+    private void Update()
+    {
+        var spriteRender = GetComponent<SpriteRenderer>();
+        var collider = GetComponent<BoxCollider2D>();
+        collider.offset = new Vector2(0, 0);
+        collider.size = new Vector3(
+            spriteRender.bounds.size.x / transform.lossyScale.x,
+            spriteRender.bounds.size.y / transform.lossyScale.y,
+            spriteRender.bounds.size.z / transform.lossyScale.z);
+    }
+}
