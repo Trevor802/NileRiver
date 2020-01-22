23db28eead380c5950ef05a8b706085a0b3e5cad
Jingyi Zhang <jerryzhangjingyi@gmail.com>
2020-01-21 23:39:01 -0800

    added player zombie sound

---
+++ Assets/Scripts/PlayerSound.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour
+{
+    public AudioSource playerAudio;
+    public AudioClip playerSound1;
+    public AudioClip playerSound2;
+    public AudioClip playerSound3;
+
+    private float delay;
+    private int soundToPlay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        delay = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delay -= Time.deltaTime;
+        if(delay <= 0f)
+        {
+            soundToPlay = Random.Range(0, 3);
+            if(soundToPlay == 1)
+            {
+                playerAudio.PlayOneShot(playerSound1);
+            }
+            else if(soundToPlay == 2)
+            {
+                playerAudio.PlayOneShot(playerSound2);
+            }
+            else if(soundToPlay == 3)
+            {
+                playerAudio.PlayOneShot(playerSound3);
+            }
+            delay = Random.Range(5, 10);
+        }
+
+    }
+}
