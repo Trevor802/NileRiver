ac6965692f152b1269374d7d5cbf8d5a03a0201f
Jingyi Zhang <jerryzhangjingyi@gmail.com>
2020-01-22 17:38:00 -0800

    redo prefab & added blood effects

--- Assets/Scripts/Dismemberment.cs
+++ Assets/Scripts/Dismemberment.cs
@@ -35,6 +35,11 @@ public class Dismemberment : MonoBehaviour
     //public Rigidbody2D head_rigid;
     private bool head_flew;
 
+    public GameObject headBlood;
+    public GameObject rightArmBlood;
+    public GameObject leftArmBlood;
+    public GameObject rightLegBlood;
+    public GameObject leftLegBlood;
 
     private Quaternion targetRotation;
     private float duration = 0f;
@@ -49,145 +54,124 @@ public class Dismemberment : MonoBehaviour
     {
         if (headCut == true && head_flew == false)
         {
+            head.transform.parent = null;
+            headBlood.SetActive(true);
+            head_flew = true;
+
             if (head.GetComponent<Rigidbody2D>())
             {
-                head.transform.parent = null;
-
                 head.GetComponent<HingeJoint2D>().enabled = false;
 
                 head.GetComponent<Rigidbody2D>().gravityScale = 0.7f;
 
                 head.GetComponent<Rigidbody2D>().AddForce(head.transform.up * thrust, ForceMode2D.Impulse);
-
-                head_flew = true;
             }
             else
             {
-                head.transform.parent = null;
-
                 Rigidbody2D head_rigid = head.AddComponent<Rigidbody2D>();
                 head_rigid.gravityScale = 0.7f;
 
                 head_rigid.simulated = true;
                 head_rigid.AddForce(head.transform.up * thrust, ForceMode2D.Impulse);
-
-                head_flew = true;
             }
         }
 
         if (rightArmCut == true && rightArm_flew == false)
         {
+            rightArm.transform.parent = null;
+            rightArmBlood.SetActive(true);
+            rightArm_flew = true;
+
             if (rightArm.GetComponent<Rigidbody2D>())
             {
-                rightArm.transform.parent = null;
-
                 rightArm.GetComponent<HingeJoint2D>().enabled = false;
 
                 rightArm.GetComponent<Rigidbody2D>().gravityScale = 0.7f;
 
                 rightArm.GetComponent<Rigidbody2D>().AddForce(head.transform.up * thrust, ForceMode2D.Impulse);
-
-                rightArm_flew = true;
             }
             else
             {
-                rightArm.transform.parent = null;
-
                 Rigidbody2D rightArm_rigid = rightArm.AddComponent<Rigidbody2D>();
                 rightArm_rigid.gravityScale = 0.7f;
 
                 rightArm_rigid.simulated = true;
                 rightArm_rigid.AddForce(rightArm.transform.up * thrust, ForceMode2D.Impulse);
                 //rightArm_hand.SetActive(false);
-
-                rightArm_flew = true;
             }
         }
 
         if (leftArmCut == true && leftArm_flew == false)
         {
+            leftArm.transform.parent = null;
+            leftArmBlood.SetActive(true);
+            leftArm_flew = true;
+
             if (leftArm.GetComponent<Rigidbody2D>())
             {
-                leftArm.transform.parent = null;
-
                 leftArm.GetComponent<HingeJoint2D>().enabled = false;
 
                 leftArm.GetComponent<Rigidbody2D>().gravityScale = 0.7f;
 
                 leftArm.GetComponent<Rigidbody2D>().AddForce(head.transform.up * thrust, ForceMode2D.Impulse);
-
-                leftArm_flew = true;
             }
             else
             {
-                leftArm.transform.parent = null;
-
                 Rigidbody2D leftArm_rigid = leftArm.AddComponent<Rigidbody2D>();
                 leftArm_rigid.gravityScale = 0.7f;
 
                 leftArm_rigid.simulated = true;
                 leftArm_rigid.AddForce(leftArm.transform.up * thrust, ForceMode2D.Impulse);
                 //leftArm_hand.SetActive(false);
-
-                leftArm_flew = true;
             } 
         }
 
         if(rightLegCut == true && rightLeg_flew == false)
         {
+            rightLeg.transform.parent = null;
+            rightLegBlood.SetActive(true);
+            rightLeg_flew = true;
+
             if (rightLeg.GetComponent<Rigidbody2D>())
             {
-                rightLeg.transform.parent = null;
-
                 rightLeg.GetComponent<HingeJoint2D>().enabled = false;
 
                 rightLeg.GetComponent<Rigidbody2D>().gravityScale = 0.7f;
 
                 rightLeg.GetComponent<Rigidbody2D>().AddForce(head.transform.up * thrust, ForceMode2D.Impulse);
-
-                rightLeg_flew = true;
             }
             else
             {
-                rightLeg.transform.parent = null;
-
                 Rigidbody2D rightLeg_rigid = rightLeg.AddComponent<Rigidbody2D>();
                 rightLeg_rigid.gravityScale = 0.7f;
 
                 rightLeg_rigid.simulated = true;
                 rightLeg_rigid.AddForce(rightLeg.transform.up * thrust, ForceMode2D.Impulse);
                 //rightLeg_foot.SetActive(false);
-
-                rightLeg_flew = true;
             }
         }
         
         if(leftLegCut == true && leftLeg_flew == false)
         {
+            leftLeg.transform.parent = null;
+            leftLegBlood.SetActive(true);
+            leftLeg_flew = true;
+
             if (leftLeg.GetComponent<Rigidbody2D>())
             {
-                leftLeg.transform.parent = null;
-
                 leftLeg.GetComponent<HingeJoint2D>().enabled = false;
 
                 leftLeg.GetComponent<Rigidbody2D>().gravityScale = 0.7f;
 
                 leftLeg.GetComponent<Rigidbody2D>().AddForce(head.transform.up * thrust, ForceMode2D.Impulse);
-
-                leftLeg_flew = true;
             }
             else
             {
-                leftLeg.transform.parent = null;
-
                 Rigidbody2D leftLeg_rigid = leftLeg.AddComponent<Rigidbody2D>();
                 leftLeg_rigid.gravityScale = 0.7f;
 
                 leftLeg_rigid.simulated = true;
                 leftLeg_rigid.AddForce(leftLeg.transform.up * thrust, ForceMode2D.Impulse);
-                //leftLeg_foot.SetActive(false);
-
-                leftLeg_flew = true;
             }
         }
         
